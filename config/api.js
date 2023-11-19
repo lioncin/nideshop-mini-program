87c852519aa5e581b06674d1883572b9075eb141
linxing <601211505@qq.com>
2023-11-19 20:28:19 +0800

    修改接口地址

--- config/api.js
+++ config/api.js
@@ -1,5 +1,5 @@
 // const ApiRootUrl = 'http://127.0.0.1:8360/api/';
-const ApiRootUrl = 'https://api.wentb.cn/';
+const ApiRootUrl = 'https://jiaoli.cloud/api/';
 // const getPhoneNumber = 'http://localhost:3000/getPhoneNumber';
 const getPhoneNumber = 'https://jiema.wentb.cn/getPhoneNumber';
 
