ca9ee7312a19e780be6c7da58ee7fbc48ccb28c2
linxing <linxing@morongai.com>
2024-02-13 10:43:42 +0800

    fx

--- pages/ucenter/orderDetail/orderDetail.js
+++ pages/ucenter/orderDetail/orderDetail.js
@@ -1,35 +1,59 @@
-var util = require('../../../utils/util.js');
-var api = require('../../../config/api.js');
+var util = require("../../../utils/util.js");
+var api = require("../../../config/api.js");
 
 Page({
   data: {
     orderId: 0,
     orderInfo: {},
     orderGoods: [],
-    handleOption: {}
+    handleOption: {},
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     this.setData({
-      orderId: options.id
+      orderId: options.id,
     });
     this.getOrderDetail();
   },
+  cancelOrder: function (e) {
+    let that = this;
+    util
+      .request(api.OrderCancel, {
+        orderId: that.data.orderId,
+      })
+      .then(function (res) {
+        if (res.errno === 0) {
+          wx.showModal({
+            title: "操作成功",
+            content: res.data,
+            showCancel: false,
+            success: function (modalRes) {
+              if (modalRes.confirm) {
+                wx.navigateBack({
+                  delta: 1,
+                });
+              }
+            },
+          });
+        }
+      });
+  },
   getOrderDetail() {
     let that = this;
-    util.request(api.OrderDetail, {
-      orderId: that.data.orderId
-    }).then(function (res) {
-      if (res.errno === 0) {
-        console.log(res.data);
-        that.setData({
-          orderInfo: res.data.orderInfo,
-          orderGoods: res.data.orderGoods,
-          handleOption: res.data.handleOption
-        });
-        //that.payTimer();
-      }
-    });
+    util
+      .request(api.OrderDetail, {
+        orderId: that.data.orderId,
+      })
+      .then(function (res) {
+        if (res.errno === 0) {
+          console.log(res.data);
+          that.setData({
+            orderInfo: res.data.orderInfo,
+            orderGoods: res.data.orderGoods,
+            handleOption: res.data.handleOption,
+          });
+        }
+      });
   },
   payTimer() {
     let that = this;
@@ -45,27 +69,28 @@ Page({
   },
   payOrder() {
     let that = this;
-    util.request(api.PayPrepayId, {
-      orderId: that.data.orderId || 15
-    }).then(function (res) {
-      if (res.errno === 0) {
-        const payParam = res.data;
-        wx.requestPayment({
-          'timeStamp': payParam.timeStamp,
-          'nonceStr': payParam.nonceStr,
-          'package': payParam.package,
-          'signType': payParam.signType,
-          'paySign': payParam.paySign,
-          'success': function (res) {
-            console.log(res)
-          },
-          'fail': function (res) {
-            console.log(res)
-          }
-        });
-      }
-    });
-
+    util
+      .request(api.PayPrepayId, {
+        orderId: that.data.orderId || 15,
+      })
+      .then(function (res) {
+        if (res.errno === 0) {
+          const payParam = res.data;
+          wx.requestPayment({
+            timeStamp: payParam.timeStamp,
+            nonceStr: payParam.nonceStr,
+            package: payParam.package,
+            signType: payParam.signType,
+            paySign: payParam.paySign,
+            success: function (res) {
+              console.log(res);
+            },
+            fail: function (res) {
+              console.log(res);
+            },
+          });
+        }
+      });
   },
   onReady: function () {
     // 页面渲染完成
@@ -78,5 +103,5 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
-  }
-})
+  },
+});
